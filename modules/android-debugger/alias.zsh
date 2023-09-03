6a6c384ee5c284b04d460181ae3108bbdd6587fd
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-09-03 12:06:14 -0400

    Fixed another hard coding error

--- modules/android-debugger/alias.zsh
+++ modules/android-debugger/alias.zsh
@@ -1,4 +1,4 @@
-alias adb="/Users/vamship/Library/Android/sdk/platform-tools/adb"
-alias adc="/Users/vamship/Library/Android/sdk/platform-tools/adb connect 192.168.43.1:5555 \
-            && /Users/vamship/Library/Android/sdk/platform-tools/adb tcpip 5555"
-alias add="/Users/vamship/Library/Android/sdk/platform-tools/adb disconnect"
+alias adb="${HOME}/Library/Android/sdk/platform-tools/adb"
+alias adc="${HOME}/Library/Android/sdk/platform-tools/adb connect 192.168.43.1:5555 \
+            && ${HOME}/Library/Android/sdk/platform-tools/adb tcpip 5555"
+alias add="${HOME}/Library/Android/sdk/platform-tools/adb disconnect"
